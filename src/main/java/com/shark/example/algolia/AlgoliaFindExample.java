694cf14bb639df802b89a65f7e60f0690eda6a60
FA00084\shark.lin <shark.lin@shoalter.com>
2023-11-29 12:10:12 +0800

    update: algolia search find example

--- src/main/java/com/shark/example/algolia/AlgoliaFindExample.java
+++ src/main/java/com/shark/example/algolia/AlgoliaFindExample.java
@@ -11,9 +11,12 @@ import java.util.List;
 
 public class AlgoliaFindExample {
     public static void main(String[] argv) {
+        //https://www.algolia.com/doc/guides/managing-results/refine-results/filtering/
         SearchClient client = DefaultSearchClient.create(AlgoliaConfig.APPLICATION_ID, AlgoliaConfig.API_KEY);
         SearchIndex<BookDto> index = client.initIndex("book", BookDto.class);
-        SearchResult<BookDto> searchResult = index.search(new Query("NAME_9").setAttributesToRetrieve(List.of("name")).setHitsPerPage(20));
+        SearchResult<BookDto> searchResult = index.search(
+                new Query("NAME_9").setAttributesToRetrieve(List.of("name", "price")).
+                        setFilters("price: 200 TO 300").setHitsPerPage(20));
         List<BookDto> bookDtoList = searchResult.getHits();
         System.out.println(new Gson().toJson(bookDtoList));
     }
