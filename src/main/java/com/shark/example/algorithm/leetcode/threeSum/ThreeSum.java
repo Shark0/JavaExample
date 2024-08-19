123f19867f22f2031e7f72eacd08a4b7f4ae2508
TW - IT - Shark <shark.lin@shoalter.com>
2024-08-19 09:02:15 +0800

    feat: leetcode

---
+++ src/main/java/com/shark/example/algorithm/leetcode/threeSum/ThreeSum.java
@@ -0,0 +1,50 @@
+package com.shark.example.algorithm.leetcode.threeSum;
+
+import com.google.gson.Gson;
+
+import java.util.*;
+
+public class ThreeSum {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> resultList = new ArrayList<>();
+        Arrays.sort(nums);
+
+        int i = 0;
+        while (i < nums.length - 2) {
+
+
+            int j = i + 1;
+            int k = nums.length - 1;
+
+            while (j < k) {
+                int result = nums[i] + nums[j] + nums[k];
+                if (result == 0) {
+                    resultList.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    while (j < k && nums[j] == nums[j + 1]) {
+                        j++;
+                    }
+                    while (j < k && nums[k] == nums[k - 1]) {
+                        k--;
+                    }
+                    j++;
+                    k--;
+                } else if (result < 0) {
+                    j++;
+                } else {
+                    k--;
+                }
+            }
+
+            while (i < nums.length - 2 && nums[i] == nums[i + 1]) {
+                i ++;
+            }
+            i++;
+        }
+        return resultList;
+    }
+
+    public static void main(String[] args) {
+        ThreeSum threeSum = new ThreeSum();
+        System.out.println(threeSum.threeSum(new int[]{-4, -2, -2, -2, 0, 1, 2, 2, 2, 3, 3, 4, 4, 6, 6}));
+    }
+}
