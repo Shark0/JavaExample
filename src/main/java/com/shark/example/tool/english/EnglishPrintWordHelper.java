6672b8302d8c1b9cd623589ac76fb70bbf89b7d5
Shark0 <lin22223344@gmail.com>
2025-02-12 17:15:16 +0800

    update: english words

--- src/main/java/com/shark/example/tool/english/EnglishPrintWordHelper.java
+++ src/main/java/com/shark/example/tool/english/EnglishPrintWordHelper.java
@@ -3,54 +3,47 @@ package com.shark.example.tool.english;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 public class EnglishPrintWordHelper {
 
-    List<String> loadWordList() {
-//        List<String> fileLst = List.of("words", "adv");
-        List<String> fileLst = List.of("words");
-        String fileName = fileLst.get(new Random().nextInt(fileLst.size()));
-        List<String> words = new ArrayList<>();
-        File file = new File("file/english/" + fileName + ".txt");
-        try (FileInputStream fileInputStream = new FileInputStream(file);
-             InputStreamReader reader = new InputStreamReader(fileInputStream)) {
-            BufferedReader bufferedReader = new BufferedReader(reader);
-            String line;
-            while ((line = bufferedReader.readLine()) != null) {
-                if (!line.isEmpty()) {
-                    words.add(line);
+    public void printWordsInFiles(int selectCount) {
+        List<String> fileLst = List.of("words", "error");
+        for (String fileName : fileLst) {
+            System.out.println("word file: " + fileName);
+            List<String> words = new ArrayList<>();
+            File file = new File("file/english/" + fileName + ".txt");
+            try (FileInputStream fileInputStream = new FileInputStream(file);
+                 InputStreamReader reader = new InputStreamReader(fileInputStream)) {
+                BufferedReader bufferedReader = new BufferedReader(reader);
+                String line;
+                while ((line = bufferedReader.readLine()) != null) {
+                    if (!line.isEmpty()) {
+                        words.add(line);
+                    }
                 }
+                int index = 0;
+                while (index < selectCount && !words.isEmpty()) {
+                    StringBuilder stringBuilder = new StringBuilder();
+                    int random = (int) (Math.random() * words.size());
+                    String word = words.get(random);
+                    words.remove(random);
+                    stringBuilder.append(word);
+                    stringBuilder.append("\n");
+                    stringBuilder.append("https://dictionary.cambridge.org/zht/%E8%A9%9E%E5%85%B8/%E8%8B%B1%E8%AA%9E-%E6%BC%A2%E8%AA%9E-%E7%B9%81%E9%AB%94/").append(word);
+                    stringBuilder.append("\n");
+                    stringBuilder.append("https://www.google.com/search?q=").append(word).append("&udm=2");
+                    stringBuilder.append("\n");
+                    index++;
+                    System.out.println(stringBuilder);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        } catch (IOException e) {
-            e.printStackTrace();
         }
-
-        return words;
-    }
-
-    public void randomSelect(List<String> words, int selectCount) {
-        int index = 0;
-        StringBuilder stringBuilder = new StringBuilder();
-        while (index < selectCount) {
-            int random = (int) (Math.random() * words.size());
-            String word = words.get(random);
-            words.remove(random);
-            stringBuilder.append(word);
-            stringBuilder.append("\n");
-            stringBuilder.append("https://dictionary.cambridge.org/zht/%E8%A9%9E%E5%85%B8/%E8%8B%B1%E8%AA%9E-%E6%BC%A2%E8%AA%9E-%E7%B9%81%E9%AB%94/").append(word);
-            stringBuilder.append("\n");
-            stringBuilder.append("https://www.google.com/search?q=").append(word).append("&udm=2");
-            stringBuilder.append("\n");
-            index++;
-        }
-        System.out.println(stringBuilder);
     }
 
     public static void main(String[] args) {
         EnglishPrintWordHelper englishHelper = new EnglishPrintWordHelper();
-        List<String> words = englishHelper.loadWordList();
-        englishHelper.randomSelect(words, Math.min(20, Math.max(words.size() / 100, 10)));
+        englishHelper.printWordsInFiles(20);
     }
-
 }
--- src/main/java/com/shark/example/tool/english/EnglishSortWordHelper.java
+++ src/main/java/com/shark/example/tool/english/EnglishSortWordHelper.java
@@ -8,20 +8,28 @@ import java.util.*;
 public class EnglishSortWordHelper {
 
     public void sort() {
+        Set<String> wordNeedDeleteSet = generateNeedDeleteWords("words_delete");
+        Set<String> errorNeedDeleteSet = generateNeedDeleteWords("error_delete");
         List<String> fileNameLst = List.of("words", "adv", "error");
         for(String fileName : fileNameLst) {
             String path = "file/english/" + fileName + ".txt";
             File file = new File(path);
             try (FileInputStream fileInputStream = new FileInputStream(file);
-                 InputStreamReader reader = new InputStreamReader(fileInputStream)
+                 InputStreamReader reader = new InputStreamReader(fileInputStream);
+                 BufferedReader bufferedReader = new BufferedReader(reader)
             ) {
-                BufferedReader bufferedReader = new BufferedReader(reader);
+
                 Set<String> wordSet = new HashSet<>();
                 String line;
                 while ((line = bufferedReader.readLine()) != null) {
                     if (!line.isEmpty()) {
-                        wordSet.add(line.trim().toLowerCase());
+                        String word = line.trim().toLowerCase();
+                        wordSet.add(word);
                     }
+                }
+                wordSet.removeAll(wordNeedDeleteSet);
+                if("error".equalsIgnoreCase(fileName)) {
+                    wordSet.removeAll(errorNeedDeleteSet);
                 }
                 List<String> wordList = new ArrayList<>(wordSet);
                 Collections.sort(wordList);
@@ -37,6 +45,26 @@ public class EnglishSortWordHelper {
             }
         }
 
+    }
+
+    public Set<String> generateNeedDeleteWords(String fileName) {
+        Set<String> set = new HashSet<>();
+        String path = "file/english/" + fileName + ".txt";
+        File file = new File(path);
+        try (FileInputStream fileInputStream = new FileInputStream(file);
+             InputStreamReader reader = new InputStreamReader(fileInputStream);
+             BufferedReader bufferedReader = new BufferedReader(reader)
+        ) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                if (!line.isEmpty()) {
+                    set.add(line.trim().toLowerCase());
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return set;
     }
 
     public static void main(String[] args) {
