b692ad9a7411a0abf380e2a87b8c1a5f98abf690
Shark0 <lin22223344@gmail.com>
2025-03-07 13:38:30 +0800

    feat: add timescaledb example

--- src/main/java/com/shark/example/service/database/timescale/TimescaleExample.java
+++ src/main/java/com/shark/example/service/database/timescale/TimescaleExample.java
@@ -1,30 +1,34 @@
-package com.shark.example.service.database.clickhouse;
+package com.shark.example.service.database.timescale;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.Random;
 
-public class ClickHouseExample {
+public class TimescaleExample {
 
     public static void main(String[] args) {
         HikariConfig config = new HikariConfig();
-        config.setJdbcUrl("jdbc:ch://localhost:8123/default");
-        config.setUsername("default");
+        config.setJdbcUrl("jdbc:postgresql://localhost:5432/postgres");
+        config.setUsername("postgres");
         config.setPassword("root");
         config.setMaximumPoolSize(10);
 
         try (HikariDataSource dataSource = new HikariDataSource(config)) {
-            int messageCount = 10;
+            int messageCount = 1;
             for(int i = 1; i <= messageCount; i++){
                 int userIdOffset = new Random().nextInt(10);
                 int messageIdOffset = new Random().nextInt(10000);
                 Connection connection = dataSource.getConnection();
                 connection.setAutoCommit(false);
-                PreparedStatement preparedStatement = connection.prepareStatement("insert into message_insert(user_id, user_service, item_id, item_service, message_id, time) values (?, ?, ?, ?, ?, ?)");
-                int userCount = 100000;
-                long start = System.currentTimeMillis();
+                PreparedStatement preparedStatement = connection.prepareStatement(
+                        "insert into message_insert(user_id, user_service, item_id, item_service, message_id, time) values (?, ?, ?, ?, ?, ?)");
+                int userCount = 10000;
+
                 for (int j = 1; j <= userCount; j++) {
                     preparedStatement.setInt(1, j + userIdOffset);
                     preparedStatement.setString(2, "HKTV");
@@ -35,11 +39,11 @@ public class ClickHouseExample {
                     preparedStatement.addBatch();
                 }
 
+                long start = System.currentTimeMillis();
                 preparedStatement.executeBatch();
                 connection.commit();
                 long end = System.currentTimeMillis();
                 System.out.println("total time: " + (end - start));
-
             }
 
         } catch (SQLException ex) {
@@ -47,6 +51,5 @@ public class ClickHouseExample {
         } catch (Exception e) {
             e.printStackTrace();
         }
-
     }
 }
